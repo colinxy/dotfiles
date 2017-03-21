c83764a2382d030c7e4d2856980b14ae03e57baa
Xinyu Yang <colin.yangxinyu@outlook.com>
2017-03-20 23:25:18 -0700

    rename .emacs to .emacs.d/init.el

--- link.sh
+++
@@ -1,5 +0,0 @@
-
-# backup and create symbolic link
-[ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak && ln -s "$PWD"/.bashrc ~/.bashrc
-[ -f ~/.emacs ] && mv ~/.emacs ~/.emacs.bak && ln -s "$PWD"/.emacs ~/.emacs
-# [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak && ln -s "$PWD"/.vimrc ~/.vimrc
