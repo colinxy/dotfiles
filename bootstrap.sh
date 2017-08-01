b2a8a9ddadd343f6761fbfc5da5019a0c7494fe7
Colin Yang <colinxy@linux.ucla.edu>
2017-08-01 12:35:14 -0700

    minor change

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # https://stackoverflow.com/a/246128
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-configs=(.bashrc .emacs.d .vimrc .emacs.min .tmux.conf)
+configs=(.bashrc .emacs.d .vimrc .emacs.min .vimrc.min .tmux.conf)
 
 for cfg in "${configs[@]}"
 do
