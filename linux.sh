7681fa09efc32fd165ae90f5f609de3135cc26ef
Colin Yang <colinxy@linux.ucla.edu>
2018-03-05 21:12:47 -0800

    update syscallnum

--- linux.sh
+++ linux.sh
@@ -4,26 +4,34 @@
 
 # source me!
 
+_platform="$(python -mplatform)"
+
+
 battery() { acpi -i; }
 
-# linux kernel
-syscallnum() {
-    [ -z "$0" ] && { echo "${FUNCNAME[0]} <syscall|syscall_number>"; return 1; }
 
-    local platform
-    platform="$(python -mplatform)"
+# linux kernel
+_syscallnum_unistd() {
+    local unistd=
+    # this file also works: <prefix>/arch/x86/entry/syscalls/syscall_64.tbl
+    echo "$_platform" | grep -qi ubuntu &&
+        unistd=/usr/src/linux-headers-$(uname -r)/include/uapi/asm-generic/unistd.h
+    echo "$_platform" | grep -qi fedora &&
+        unistd=/usr/src/kernels/$(uname -r)/include/uapi/asm-generic/unistd.h
+
+    [ -z "$unistd" ] && { echo "Unknown platform: $_platform" >&2; return 2; }
+    echo "$unistd"
+}
+
+syscallnum() {
+    [ -z "$1" ] && { echo "${FUNCNAME[0]} <syscall|syscall_number>" >&2; return 1; }
 
     local unistd
-    # this file also works: <prefix>/arch/x86/entry/syscalls/syscall_64.tbl
-    echo "$platform" | grep -qi ubuntu &&
-        unistd=/usr/src/linux-headers-$(uname -r)/include/uapi/asm-generic/unistd.h
-    echo "$platform" | grep -qi fedora &&
-        unistd=/usr/src/kernels/$(uname -r)/include/uapi/asm-generic/unistd.h
-    [ -z "$unistd" ] && { echo "Unknown platform: $platform"; return 2; }
+    unistd=$(_syscallnum_unistd) || return 2
 
     if [[ "$1" =~ ^[0-9]+$ ]]; then
         grep -w "$1" "$unistd"
     else
-        grep -w -B1 "sys_$1" "$unistd"
+        grep -w -B1 "sys_$1" "$unistd" || grep -B1 "$1" "$unistd"
     fi
 }
