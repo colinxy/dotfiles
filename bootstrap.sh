41b9d5c3c5999e2117bef3f0a6ffaaf7c37f8647
Colin Yang <colinxy@linux.ucla.edu>
2019-01-24 12:45:54 -0800

    improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
 
+set -e
+
 # https://stackoverflow.com/a/246128
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 configs=(
     .inputrc
     .bashrc
-    .emacs.d
-    .vimrc
+    .bash_colors
+    .pythonrc
     .emacs.min
     .vimrc.min
-    .tmux.conf
-    .pythonrc
 )
 
-for cfg in "${configs[@]}"
+linknames=(
+    .inputrc
+    .bashrc
+    .bash_colors
+    .pythonrc
+    .emacs.min
+    .vimrc.min
+)
+
+for arg in "$@"
 do
-    [ -r "$HOME/$cfg" ] && mv "$HOME/$cfg" "$HOME/$cfg".bak
-    ln -s "$DIR/$cfg" "$HOME/$cfg"
+    case "$arg" in
+        vimrc)
+            configs+=(".vimrc")
+            linknames+=(".vimrc");;
+        vimrc.min)
+            configs+=(".vimrc.min")
+            linknames+=(".vimrc");;
+        emacs)
+            configs+=(".emacs.d")
+            linknames+=(".emacs.d");;
+        emacs.remote)
+            configs+=(".emacs.remote.d")
+            linknames+=(".emacs.d");;
+        tmux)
+            configs+=(".tmux.conf")
+            linknames+=(".tmux.conf");;
+    esac
+done
+
+
+for ((i=0; i<${#configs[@]}; i++))
+do
+    cfg="${configs[i]}"
+    link="${linknames[i]}"
+    [ -r "$HOME/$link" ] && mv -n "$HOME/$link" "$HOME/$link".bak
+    ln -s "$DIR/$cfg" "$HOME/$link"
 done
