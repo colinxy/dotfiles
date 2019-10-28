ee3917baa30368cae3caa9e8a7d3151d28138c54
Colin Yang <colinxy@linux.ucla.edu>
2019-10-27 18:41:59 -0700

    remove org mode S-keys

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -480,6 +480,10 @@ stack overflow answer https://stackoverflow.com/a/18885461."
   (setq org-agenda-files '("~/org"))
   (setq org-default-notes-file "~/org/notes.org")
   :config
+  (define-key org-mode-map (kbd "S-<left>") nil)
+  (define-key org-mode-map (kbd "S-<right>") nil)
+  (define-key org-mode-map (kbd "S-<up>") nil)
+  (define-key org-mode-map (kbd "S-<down>") nil)
   (setq org-src-fontify-natively t)
   (setq org-fontify-whole-heading-line t)
   (setq org-src-tab-acts-natively t)
